6ad8cdb622d06471ef8a4b461ee7e4ec810cb98e
Nguyen Minh Triet <minhtrietk2000@gmail.com>
2023-06-10 22:06:51 +0700

    Adding DynamicArray-AbstractDataType

---
+++ DynamicArray__ADT/DynamicMain.java
@@ -0,0 +1,12 @@
+package DynamicArray__ADT;
+
+public class DynamicMain {
+    public static void main(String[] args) {
+        DynamicArr<String> arr = new DynamicArr<>();
+        arr.add("Triet");
+        arr.add("Nguyen");
+        arr.add("Minh");
+        System.out.println(arr.toString());
+        System.out.println(arr.size());
+    }
+}
